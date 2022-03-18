664d2274d5475223b80a18bf93b5ee94a75647b5
misternano <cjstitt@icloud.com>
2022-03-18 07:58:23 -0400

    minor improvements

--- js/greet.js
+++ js/greet.js
@@ -2,5 +2,4 @@ $(document).ready(function() {
     var text = ["Hi, I'm Nano :)", "Hey, I'm Nano :)", "Hello, I'm Nano :)"];
     var x = Math.floor((Math.random() * 3));
     $('#greet').html(text[x]);
-
 });
