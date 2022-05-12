a3c1145e3a50e820f19ff8ee8ee5f764d4f5a3de
misternano <cjstitt@icloud.com>
2022-05-11 21:07:22 -0400

    Update quote.js

--- js/quote.js
+++ js/quote.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    var text = ["placeholder"];
-    var x = Math.floor((Math.random() * 1));
-    $('#greet').html(text[x]);
+    var text = ["maybe the next step in your healing journey is just telling more people to fuck right off."];
+    var x = Math.floor((Math.random() * 1)); /* change number to fit how many quotes are in the array */ 
+    $('#quote').html(text[x]);
 });
