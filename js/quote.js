d6067947b67a7e0ef76cd3463197f6b7e7b27254
misternano <cjstitt@icloud.com>
2022-05-12 18:52:02 -0400

    added random quotes to home page, updated projects image display

--- js/quote.js
+++ js/quote.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
-    var text = ["maybe the next step in your healing journey is just telling more people to fuck right off.", "sometimes the reason good things aren't happening to you is because you're a huge fucking cunt."];
-    var x = Math.floor((Math.random() * 2)); /* change number to fit how many quotes are in the array */ 
+    var text = [
+        "maybe the next step in your healing journey is telling more people to fuck right off.",
+        "sometimes the reason good things aren't happening to you is because you're a huge fucking cunt.",
+        "amazing things will happen today if you decide not to be a miserable cunt.",
+        "maybe the only reason you think you're not good enough is because you're surrounded by a bunch of cunts."
+    ];
+    var x = Math.floor((Math.random() * 4)); /* change number to fit how many quotes are in the array */
     $('#quote').html(text[x]);
 });
