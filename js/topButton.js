84816b700f261d0c34000ad371a83ba8c2790948
misternano <cjstitt@icloud.com>
2022-06-19 13:15:01 -0400

    General fixes

--- js/topButton.js
+++ js/topButton.js
@@ -1,18 +1,15 @@
-//Get the button
 var mybutton = document.getElementById("topBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() { scrollFunction() };
 
 function scrollFunction() {
-    if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
+    if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) { // 250px
         mybutton.style.display = "block";
     } else {
         mybutton.style.display = "none";
     }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
