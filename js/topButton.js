15491c57b6aa841dbb760f1e28582cdd91331790
misternano <cjstitt@icloud.com>
2022-09-01 20:09:13 -0400

    projects page rewritten

--- js/topButton.js
+++ js/topButton.js
@@ -3,7 +3,7 @@ var mybutton = document.getElementById("topBtn");
 window.onscroll = function() { scrollFunction() };
 
 function scrollFunction() {
-    if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) { // 250px
+    if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 199) { // 199px until button appears
         mybutton.style.display = "block";
     } else {
         mybutton.style.display = "none";
