4e69fbe1fe3a413ba06e85501c7711c981ebaeb3
misternano <cjstitt@icloud.com>
2022-09-02 18:42:03 -0400

    projects page updated, nojs screen optimized, placeholder image size fixed

---
+++ js/buttons.js
@@ -0,0 +1,21 @@
+var topbutton = document.getElementById("topBtn");
+
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 1000) { // 1299px until button appears
+        topbutton.style.display = "block";
+    } else {
+        topbutton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+// FADE AWAY BOUNCING BUTTON ON PROJECTS
+$(window).scroll(function() {
+    $("#projectsBtn").css("opacity", 1 - $(window).scrollTop() / 500);
+});
