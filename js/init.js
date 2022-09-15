5e9b1598ebbadf0ce61d3f4686b9760157820ea0
misternano <cjstitt@icloud.com>
2022-09-15 18:39:03 -0400

    clean up + mobile visual changes

--- js/init.js
+++ js/init.js
@@ -1,16 +1,11 @@
 // INITIALISE RESOURCES
-console.log("%cfuck off 🌷", "color:#e8a0e3;font-size:14px;padding:14px;font-family:monospace;")
+console.log("%cfuck off 🌷", "color:#e8a0e3;font-size:14px;padding:14px;font-family:monospace;");
 
-// UPDATE NAVBAR AVATAR
+// CHANGE GREETING
 $(document).ready(function() {
-    fetch("https://api.lanyard.rest/v1/users/272535850200596480")
-        .then((response) => response.json())
-        .then((json) => {
-            const avatarHash = json.data.discord_user.avatar;
-            const url = `https://cdn.discordapp.com/avatars/272535850200596480/${avatarHash}.png`;
-
-            $('#avatar').attr("src", url);
-        });
+    const text = ["Hi", "Hey", "Hello"];
+    const x = Math.floor((Math.random() * text.length));
+    $('#greet').html(text[x]);
 });
 
 // START AOS
