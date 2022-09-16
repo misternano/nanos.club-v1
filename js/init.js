57a3199968afe5c07f21504dd7575cb8dacfca72
misternano <cjstitt@icloud.com>
2022-09-15 20:41:08 -0400

    fixed oopsies

--- js/init.js
+++ js/init.js
@@ -8,6 +8,29 @@ $(document).ready(function() {
     $('#greet').html(text[x]);
 });
 
+// UPDATE NAVBAR AVATAR
+$(document).ready(function() {
+    fetch("https://api.lanyard.rest/v1/users/272535850200596480")
+        .then((response) => response.json())
+        .then((json) => {
+            const avatarHash = json.data.discord_user.avatar;
+            const status = json.data.discord_status;
+
+            const url = `https://cdn.discordapp.com/avatars/272535850200596480/${avatarHash}.png`;
+            $('#avatar').attr("src", url);
+
+            if (status == "online") {
+                $('#avatar').css("border-color", "#43b581");
+            } else if (status == "idle") {
+                $('#avatar').css("border-color", "#faa61a");
+            } else if (status == "dnd") {
+                $('#avatar').css("border-color", "#f04747");
+            } else {
+                $('#avatar').css("border-color", "#747f8d");
+            }
+        });
+});
+
 // START AOS
 AOS.init({
     // global settings
