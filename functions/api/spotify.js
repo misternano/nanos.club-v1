3ac9d490ced278bb6ca81c55cb924df0f9bdb149
misternano <cjstitt@icloud.com>
2022-09-17 15:39:36 -0400

    L bozo v2

--- functions/api/spotify.js
+++ functions/api/spotify.js
@@ -1,12 +1,16 @@
 export const onRequestGet = async({ env }) => {
-    const tokens = await env.TOKENS.get("tokens", { type: "json" });
-    const response = await request("https://api.spotify.com/v1/me/player/currently-playing", tokens.accessToken, tokens.refreshToken, env.CLIENT_ID, env.CLIENT_SECRET);
+    try {
+        const tokens = await env.TOKENS.get("tokens", { type: "json" });
+        const response = await request("https://api.spotify.com/v1/me/player/currently-playing", tokens.accessToken, tokens.refreshToken, env.CLIENT_ID, env.CLIENT_SECRET);
 
-    return new Response(JSON.stringify(response), {
-        headers: {
-            "content-type": "application/json"
-        }
-    });
+        return new Response(JSON.stringify(response), {
+            headers: {
+                "content-type": "application/json"
+            }
+        });
+    } catch (err) {
+        return new Response(err, { status: 500 });
+    }
 };
 
 const refreshToken = async(refreshToken, CLIENT_ID, CLIENT_SECRET) => {
